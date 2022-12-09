e7f999b106d4b471dc74d899de173e1097532e83
Huu Quang Linh Nguyen <quanglinh.nguyen@rikai.technology>
2022-12-09 11:51:35 +0700

    push code

---
+++ src/common/header/Head.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Head(props) {
+    return (
+        <>
+            <section className="head">
+                <div className="container d_flex"></div>
+            </section>
+        </>
+    );
+}
+
+export default Head;
